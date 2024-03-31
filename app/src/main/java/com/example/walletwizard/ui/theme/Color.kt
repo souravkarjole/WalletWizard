7aae4fef531de0da770e576e1a1ece2a615c2c6c
souravkarjole <113442535+souravkarjole@users.noreply.github.com>
2024-03-31 12:10:37 +0530

    Initial

--- app/src/main/java/com/example/walletwizard/ui/theme/Color.kt
+++ app/src/main/java/com/example/walletwizard/ui/theme/Color.kt
@@ -1,6 +1,15 @@
 package com.example.walletwizard.ui.theme
 
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.example.walletwizard.DarkMode
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -10,12 +19,17 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val DarkSapphire = Color(0xFF32367C)
-val LightSapphire = Color(0xFFA0A4C7)
-val ExtraDarkTransparent = Color(0x40000000)
-val LightestSapphire = Color(0xFFE3E8F3)
-val LightAliceBlue = Color(0xFFEEEEF8)
+val DarkModeDarkSapphire = Color(0xFF2F213C)
+val DarkModeLightSapphire = Color(0xFFDDD6E4)
+val DarkModeLightestSapphire = Color(0xFFE1DAEB)
+val DarkModeLightAliceBlue = Color(0xFFF5EDFC)
 
+val LightModeDarkSapphire = Color(0xFF32367C)
+val LightModeLightSapphire = Color(0xFFA0A4C7)
+val LightModeLightestSapphire = Color(0xFFE3E8F3)
+val LightModeLightAliceBlue = Color(0xFFEEEEF8)
+
+val ExtraDarkTransparent = Color(0x40000000)
 val Green = Color(0xFF098D09)
 
 // -----------------------------
@@ -37,3 +51,43 @@ val Emerald = 0xFF50C878
 val Salmon = 0xFFFA8072
 val Teal = 0xFF008080
 
+
+
+@Composable
+fun observeDarkModeState(darkMode: DarkMode): MutableState<Boolean> {
+    val isDarkMode = remember { mutableStateOf(false) }
+
+    LaunchedEffect(darkMode) {
+       isDarkMode.value = darkMode.isDarkModeEnabled()
+    }
+
+    return isDarkMode
+}
+
+
+@Composable
+fun getColorPalette(context: Context):ColorPalette{
+    val darkMode:DarkMode = DarkMode.getInstance(context)
+    val isDarkMode by observeDarkModeState(darkMode = darkMode)
+
+    if(isDarkMode){
+        return ColorPalette(
+            Color.White,
+            DarkModeDarkSapphire,DarkModeDarkSapphire, DarkModeLightSapphire, DarkModeLightestSapphire,
+            DarkModeLightAliceBlue)
+    }else{
+        return ColorPalette(
+            LightModeDarkSapphire,
+            Color.White,LightModeDarkSapphire, LightModeLightSapphire, LightModeLightestSapphire,
+            LightModeLightAliceBlue)
+    }
+}
+
+data class ColorPalette(
+    val unselectedColor:Color,
+    val surfaceColor:Color,
+    val darkSapphire:Color,
+    val lightSapphire:Color,
+    val lightestSapphire:Color,
+    val lightAliceSapphire:Color
+)
