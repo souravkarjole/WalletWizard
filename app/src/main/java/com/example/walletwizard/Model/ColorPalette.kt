7f1e4e044e9d68006bb36fddc5578b3cc571cb5e
souravkarjole <113442535+souravkarjole@users.noreply.github.com>
2024-04-05 15:28:14 +0530

    Initial

---
+++ app/src/main/java/com/example/walletwizard/Model/ColorPalette.kt
@@ -0,0 +1,12 @@
+package com.example.walletwizard.Model
+
+import androidx.compose.ui.graphics.Color
+
+data class ColorPalette(
+    val unselectedColor: Color,
+    val surfaceColor: Color,
+    val darkSapphire: Color,
+    val lightSapphire: Color,
+    val lightestSapphire: Color,
+    val lightAliceSapphire: Color
+)
